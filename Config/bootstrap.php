38064b9d02d806e016e2340f5f59b288a8ee91bc
Gilles Wittenberg <git@gilleswittenberg.com>
2013-09-07 17:41:05 +0200

    Improve bootstrap

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
-Configure::write('Apps.dbConfig', 'apps');
+Configure::write('Apps.dbConfig', 'default');
 Configure::write('Apps.domain', 'example.com');
 Configure::write('Apps.httpdRoot', '/etc/apache2');
 Configure::write('Apps.restartCmd', '/usr/bin/sudo /usr/sbin/apachectl graceful');
 Configure::write('Apps.sqlDir', 'Plugin' . DS . 'Apps' . DS . 'Config' . DS . 'Schema');
 Configure::write('Apps.schemaFile', 'schema.php');
 //Configure::write('Apps.tablesFile', 'rows.sql');
-Configure::write('Apps.configDir', 'Config' . DS . 'applications');
+Configure::write('Apps.configDir', APP_DIR . DS . 'Config' . DS . 'applications');
 Configure::write('Apps.dumpDir', TMP);
 Configure::write('Apps.cakePath', 'lib' . DS . 'Cake' . DS . 'Console' . DS . 'cake');
