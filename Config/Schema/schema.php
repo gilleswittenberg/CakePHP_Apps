53ab9a98af2276932eb0c086578724bac35492a5
Gilles Wittenberg <git@gilleswittenberg.com>
2013-09-10 22:10:10 +0200

    Set Application.status default value to 1

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -12,7 +12,8 @@ class AppSchema extends CakeSchema {
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 99, 'key' => 'primary'),
 		'document_root_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 99),
 		'server_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 199, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'status' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 199, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'slug' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 199, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'status' => array('type' => 'string', 'null' => false, 'default' => '1', 'length' => 199, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
