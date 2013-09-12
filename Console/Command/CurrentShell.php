e2c1fcc52f373761cd05c0c751ac9e389f22e4e9
Gilles Wittenberg <git@gilleswittenberg.com>
2013-09-12 17:59:14 +0200

    Add tests for CurrentShell

--- Console/Command/CurrentShell.php
+++ Console/Command/CurrentShell.php
@@ -1,29 +1,40 @@
 <?php
+App::uses('AppShell', 'Console/Command');
 class CurrentShell extends AppShell {
 
 	public function main() {
 		if (empty($this->args[0])) {
-			$this->error('Supply document_root');
+			return $this->err('Supply document_root');
 		}
 		$documentRoot = $this->args[0];
 		// print current application
 		if (empty($this->args[1])) {
-			$file = new File($documentRoot . DS . Configure::read('Apps.configDir') . DS . 'current_application');
-			if ($file->exists()) {
-				//echo exec('readlink ' . APP . Configure::read('Apps.configDir') . DS . 'current_application');
-				echo exec('readlink ' . $documentRoot . DS . Configure::read('Apps.configDir') . DS . 'current_application');
+			$fileName = $documentRoot . DS . Configure::read('Apps.configDir') . DS . 'current_application';
+			if ($this->fileExists($fileName)) {
+				echo $this->exec('readlink ' . $fileName);
+				return;
 			} else {
-				$this->err('current_application does not exist');
+				return $this->err('current_application does not exist');
 			}
 		}
 		// link current application
 		else {
-			$file = new File($documentRoot . DS . Configure::read('Apps.configDir') . DS . $this->args[1] . '.php');
-			if ($file->exists()) {
-				exec('ln -fs ' . $documentRoot . DS . Configure::read('Apps.configDir') . DS . $this->args[1] . '.php ' . $documentRoot . DS . Configure::read('Apps.configDir') . DS . 'current_application');
+			$fileName = $documentRoot . DS . Configure::read('Apps.configDir') . DS . $this->args[1] . '.php';
+			if ($this->fileExists($fileName)) {
+				$this->exec('ln -fs ' . $fileName . ' ' . $documentRoot . DS . Configure::read('Apps.configDir') . DS . 'current_application');
+				return;
 			} else {
-				$this->err('Not an application');
+				return $this->err('Not an application');
 			}
 		}
+	}
+
+	protected function fileExists($fileName) {
+		$file = new File($fileName);
+		return $file->exists();
+	}
+
+	protected function exec($command) {
+		return exec($command);
 	}
 }
---
+++ Test/Case/Console/Command/CurrentShellTest.php
@@ -0,0 +1,90 @@
+<?php
+
+App::uses('ShellDispatcher', 'Console');
+App::uses('ConsoleOutput', 'Console');
+App::uses('ConsoleInput', 'Console');
+App::uses('Shell', 'Console');
+App::uses('CurrentShell', 'Apps.Console/Command');
+
+
+/**
+ * CurrentShellTest class
+ *
+ * @package       Plugin.Apps.Test.Case.Console.Command
+ */
+class CurrentShellTest extends CakeTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.apps.document_root', 'plugin.apps.application'
+	);
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		$out = $this->getMock('ConsoleOutput', array(), array(), '', false);
+		$in = $this->getMock('ConsoleInput', array(), array(), '', false);
+		$this->Shell = $this->getMock(
+			'CurrentShell',
+			array('in', 'out', 'hr', 'createFile', 'error', 'err', '_stop', 'fileExists', 'exec'),
+			array($out, $out, $in)
+		);
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		parent::tearDown();
+		if (!empty($this->file) && $this->file instanceof File) {
+			$this->file->delete();
+			unset($this->file);
+		}
+	}
+
+	public function testEmptyArgs() {
+		$this->Shell->expects($this->once())
+			->method('err');
+		$this->Shell->main();
+	}
+
+	public function testNonExistingDocumentRoot() {
+		$this->Shell->expects($this->once())
+			->method('err');
+		$this->Shell->args = array('test');
+		$this->Shell->main();
+	}
+
+	public function testExistingDocumentRoot() {
+		$this->Shell->expects($this->never())
+			->method('err');
+		$this->Shell->expects($this->once())
+			->method('fileExists')
+			->will($this->returnValue(true));
+		$this->Shell->args = array(APP);
+		$this->Shell->main();
+	}
+
+	public function testLinking() {
+		$this->Shell->expects($this->never())
+			->method('err');
+		$this->Shell->expects($this->once())
+			->method('fileExists')
+			->will($this->returnValue(true));
+		$this->Shell->expects($this->once())
+			->method('exec');
+		$this->Shell->args = array(APP, 'application-9.example.com');
+		$this->Shell->main();
+	}
+}
