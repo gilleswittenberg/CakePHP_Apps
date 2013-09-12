69282c968eb2e25a991ad5b3d4a3a26f25a8c99d
Gilles Wittenberg <git@gilleswittenberg.com>
2013-09-12 21:52:53 +0200

    Add tests for CreateApplicationShell

--- Console/Command/CreateApplicationShell.php
+++ Console/Command/CreateApplicationShell.php
@@ -1,12 +1,18 @@
 <?php
+App::uses('AppShell', 'Console/Command');
 class CreateApplicationShell extends AppShell {
 
 	public $uses = array('DocumentRoot', 'Application');
 
 	public function main() {
+		if (empty($this->args[0])) {
+			$this->error('Supply DocumentRoot');
+			return false;
+		}
 		$documentRoot = $this->DocumentRoot->find('first', array('conditions' => array('absolute_path' => $this->args[0])));
 		if (!$documentRoot) {
 			$this->error('No valid DocumentRoot');
+			return false;
 		}
 		$this->Application->saveAssociated(array('Application' => array('document_root_id' => $documentRoot['DocumentRoot']['id']), 'Database' => array('id' => '')));
 	}
---
+++ Test/Case/Console/Command/CreateApplicationTest.php
@@ -0,0 +1,75 @@
+<?php
+
+App::uses('ShellDispatcher', 'Console');
+App::uses('ConsoleOutput', 'Console');
+App::uses('ConsoleInput', 'Console');
+App::uses('Shell', 'Console');
+App::uses('CreateApplicationShell', 'Apps.Console/Command');
+
+
+/**
+ * CurrentShellTest class
+ *
+ * @package       Plugin.Apps.Test.Case.Console.Command
+ */
+class CreateApplicationShellTest extends CakeTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.apps.document_root', 'plugin.apps.application', 'plugin.apps.database', 'plugin.apps.server_alias'
+	);
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		$out = $this->getMock('ConsoleOutput', array(), array(), '', false);
+		$in = $this->getMock('ConsoleInput', array(), array(), '', false);
+		$this->Shell = $this->getMock(
+			'CreateApplicationShell',
+			array('in', 'out', 'hr', 'createFile', 'error', 'err', '_stop'),
+			array($out, $out, $in)
+		);
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		parent::tearDown();
+		if (!empty($this->file) && $this->file instanceof File) {
+			$this->file->delete();
+			unset($this->file);
+		}
+	}
+
+	public function testEmptyArgs() {
+		$this->Shell->expects($this->once())
+			->method('error');
+		$this->Shell->main();
+	}
+
+	public function testNonExistingDocumentRoot() {
+		$this->Shell->expects($this->once())
+			->method('error');
+		$this->Shell->args = array('/non_existing/path');
+		$this->Shell->main();
+	}
+
+	public function testExistingDocumentRoot() {
+		$this->Shell->expects($this->never())
+			->method('error');
+		$this->Shell->args = array(APP);
+		$this->Shell->main();
+	}
+}
