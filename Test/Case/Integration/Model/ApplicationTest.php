acafb3074f5cffac923e291431781d971d4dee2d
Gilles Wittenberg <git@gilleswittenberg.com>
2013-09-14 19:40:16 +0200

    Add ServerAlias to Application Integration test

--- Test/Case/Integration/Model/ApplicationTest.php
+++ Test/Case/Integration/Model/ApplicationTest.php
@@ -41,13 +41,16 @@ class IntegrationApplicationTest extends CakeTestCase {
 	public function testCreateAndDelete() {
 		// create
 		$this->Application->create();
-		$this->Application->saveAssociated(array('Application' => array('document_root_id' => 1), 'Database' => array('id' => '')));
+		$this->Application->saveAssociated(array('Application' => array('document_root_id' => 1), 'Database' => array('id' => ''), 'ServerAlias' => array(array('domain' => 'a.example.com'))));
 		$this->Application->init();
 		$id = $this->Application->id;
 		$this->assertNotEmpty($this->Application->query("SHOW DATABASES LIKE 'application-$id'", false));
 		$this->assertNotEmpty($this->Application->query("SELECT USER FROM mysql.user WHERE User='application-$id'", false));
 		$file = new File(APP . Configure::read('Apps.configDir') . DS . 'application-' . $id . '.' . Configure::read('Apps.domain') . '.php');
 		$this->assertTrue($file->exists());
+		$fileServerAlias = new File(APP . Configure::read('Apps.configDir') . DS . 'a.example.com.php');
+		$this->assertTrue($fileServerAlias->exists());
+		$this->assertEquals($file->read(), $fileServerAlias->read());
 		$file = new File(Configure::read('Apps.httpdRoot') . DS . 'sites-available' . DS . 'application-' . $id . '.' . Configure::read('Apps.domain'));
 		$this->assertTrue($file->exists());
 
@@ -57,6 +60,8 @@ class IntegrationApplicationTest extends CakeTestCase {
 		$this->assertEmpty($this->Application->query("SELECT USER FROM mysql.user WHERE User='application-$id'", false));
 		$file = new File(APP . Configure::read('Apps.configDir') . DS . 'application-' . $id . '.' . Configure::read('Apps.domain') . '.php');
 		$this->assertFalse($file->exists());
+		$fileServerAlias = new File(APP . Configure::read('Apps.configDir') . DS . 'a.example.com.php');
+		$this->assertFalse($fileServerAlias->exists());
 		$file = new File(Configure::read('Apps.httpdRoot') . DS . 'sites-available' . DS . 'application-' . $id . '.' . Configure::read('Apps.domain'));
 		$this->assertFalse($file->exists());
 	}
